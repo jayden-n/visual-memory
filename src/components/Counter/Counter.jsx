c7b8ed0e053353c86e2a1fa53a2568a6bd87c1ae
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-08-23 20:32:13 -0400

    created: counting amount of cards

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -3,8 +3,36 @@ import PropTypes from 'prop-types';
 
 import './Counter.css';
 
-const Counter = () => {
-    return <p>Counter</p>;
+const STEP = 2;
+
+const Counter = ({ cardsCount, onClick }) => {
+  const onDecrease = (e) => {
+    e.preventDefault();
+
+    onClick((cardsCount) => Math.max(cardsCount - STEP, 2));
+  };
+
+  const onIncrease = (e) => {
+    e.preventDefault();
+    onClick((cardsCount) => Math.min(cardsCount + STEP, 160));
+  };
+
+  return (
+    <div className='quantity'>
+      <button className='minus' onClick={onDecrease}>
+        -
+      </button>
+      <span className='quantity'>{cardsCount}</span>
+      <button className='plus' onClick={onIncrease}>
+        +
+      </button>
+    </div>
+  );
 };
 
 export default Counter;
+
+Counter.propTypes = {
+  cardsCount: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,14 +1,16 @@
 import React, { Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { CATEGORIES } from '../../constants';
+import { CATEGORIES, INITIAL_CARDS_COUNT, LEVEL } from '../../constants';
 
 import styles from './Settings.module.css';
 import RadioBox from '../RadioBox';
+import Counter from '../Counter/Counter';
 
 const Settings = () => {
   const [category, setCategory] = useState(CATEGORIES[0]);
-
+  const [level, setLevel] = useState(LEVEL[0]);
+  const [cardsCount, setCardsCount] = useState(INITIAL_CARDS_COUNT);
   return (
     <div className={`${styles.settings} frosted`}>
       <h2>Settings</h2>
@@ -22,6 +24,21 @@ const Settings = () => {
             key={item}
             selectedItem={category}
             onChange={(e) => setCategory(e.target.value)}
+          />
+        ))}
+      </div>
+      <h4>Amount of cards:</h4>
+      <div className={`${styles.setting}`}>
+        <Counter cardsCount={cardsCount} onClick={setCardsCount} />
+      </div>
+      <h4>Level:</h4>
+      <div className={`${styles.setting}`}>
+        {LEVEL.map((item) => (
+          <RadioBox
+            item={item}
+            key={item}
+            selectedItem={level}
+            onChange={(e) => setLevel(e.target.value)}
           />
         ))}
       </div>
